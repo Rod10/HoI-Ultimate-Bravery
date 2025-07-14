a13eefcb465d6121eae4172310345cef1e20cb3f
Rod24 <erwan.billy@hotmail.fr>
2025-07-14 16:19:04 +0200

    add Classes files

---
+++ HoI Ultimate Bravery/Classes/Ideas/Idea.cs
@@ -0,0 +1,20 @@
+﻿namespace HoI_Ultimate_Bravery.Classes.Ideas
+{
+    public class Idea
+    {
+        public Dictionary<string, object>? Allowed = new Dictionary<string, object>();
+        public Dictionary<string, object>? AllowedCivilWar = new Dictionary<string, object>();
+        public int? RemovalCost;
+        public Dictionary<string, object>? Modifier = new Dictionary<string, object>();
+        public Dictionary<string, object>? Rule = new Dictionary<string, object>();
+        public Dictionary<string, object>? ResearchBonus = new Dictionary<string, object>();
+        public Dictionary<string, object>? EquipmentBonus = new Dictionary<string, object>();
+        public Dictionary<string, object>? Cancel = new Dictionary<string, object>();
+        public Dictionary<string, object>? OnAdd = new Dictionary<string, object>();
+        public Dictionary<string, object>? OnRemove = new Dictionary<string, object>();
+        public Dictionary<string, object>? DoEffect = new Dictionary<string, object>();
+        public Dictionary<string, object>? TargetedModifier = new Dictionary<string, object>();
+        public string? Picture;
+        public string? Name;
+    }
+}
---
+++ HoI Ultimate Bravery/Classes/Ideas/Ideas.cs
@@ -0,0 +1,14 @@
+﻿using HoI_Ultimate_Bravery.Classes.Ideas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoI_Ultimate_Bravery.Classes.Ideas
+{
+    public class Ideas
+    {
+        public Dictionary<string, IdeasDictionnary> ideas = new Dictionary<string, IdeasDictionnary>();
+    }
+}
